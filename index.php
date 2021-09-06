d32e8965346c0a4c2c532f2513fb90b671d35e0b
zatulsuhaida <zatulsuhaida98@yahoo.com>
2021-09-06 12:11:19 +0800

    edit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SKS</title>
+</head>
+
+<body>
+<pre>
+<center>
+<h2>DFP4063 Projek Web Programming</h2>
+        
+<h1>Nama Sistem : Sistem Kereta Sewa (SKS)</h1>
+            
+<a href="manager/index.php">Manager (Zatul)</a>
+       
+<a href="staff/index.php">Staff (Radzi)</a>
+          
+<a href="customer/index.php">Customer (Syakir)</a>
+
+<hr></hr> 
+
+</body>
+</html>
