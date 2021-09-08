451592bf84785f1eb4311bb77daa31edc32f3510
zatulsuhaida <zatulsuhaida98@yahoo.com>
2021-09-08 09:56:01 +0800

    edit file

--- customer/index.php
+++ customer/index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customer</title>
+    <title>SKS - customer</title>
     <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
-<pre>
+    <pre>
 <center>
 <h2>Customer (Syakir)</h2>
           
