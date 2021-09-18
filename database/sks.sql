40b6a31957606abb5e68e8a4052e371bdbff67b4
Bukankir18 <garenasyakir@gmail.com>
2021-09-18 23:16:15 +0800

    try edit index

--- database/sks.sql
+++ database/sks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2021 at 01:53 PM
+-- Generation Time: Sep 18, 2021 at 05:12 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -24,16 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kereta`
+--
+
+CREATE TABLE `kereta` (
+  `idkereta` int(10) NOT NULL,
+  `modal` varchar(20) NOT NULL,
+  `priceperday` varchar(10) NOT NULL,
+  `priceperweek` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kereta`
+--
+
+INSERT INTO `kereta` (`idkereta`, `modal`, `priceperday`, `priceperweek`) VALUES
+(1, 'saga', 'RM 100', 'RM 700'),
+(2, 'Myvi', 'RM 80', 'RM 560'),
+(3, 'Alza', 'RM 120', 'RM 840'),
+(4, 'Axia', 'RM 60', 'RM 420');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tempahan`
 --
 
 CREATE TABLE `tempahan` (
-  `idtempahan` int(10) NOT NULL,
-  `namapenyewa` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `idtempahan` int(30) NOT NULL,
+  `namapenyewa` varchar(30) NOT NULL,
   `notelefon` int(10) NOT NULL,
-  `email` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(20) NOT NULL,
   `noIC` int(20) NOT NULL,
-  `alamat` varchar(30) NOT NULL,
+  `alamat` varchar(20) NOT NULL,
   `jeniskereta` varchar(10) NOT NULL,
   `tarikhambil` varchar(20) NOT NULL,
   `masaambil` varchar(20) NOT NULL,
@@ -47,27 +70,27 @@ CREATE TABLE `tempahan` (
 
 INSERT INTO `tempahan` (`idtempahan`, `namapenyewa`, `notelefon`, `email`, `noIC`, `alamat`, `jeniskereta`, `tarikhambil`, `masaambil`, `tarikhhantar`, `destinasi`) VALUES
 (1, 'Syakir Ridzuan', 175659039, 'syakir@gmail.com', 218020689, 'No 24 Taman Sri Jaya', 'Myvi', '18 Jun 2021', '8:30 AM', '20 Jun 2021', 'Penang'),
-(2, 'Kamarul Nizam', 115659039, 'nizam@gmail.com', 218020689, 'No 24 Taman Sri Jaya', 'Myvi', '18 Jun 2021', '8:30 AM', '20 Jun 2021', 'Penang');
+(2, 'Kamarul Nizam', 115659039, 'nizam@gmail.com', 224140687, 'No 24 Taman Mawar', 'Saga', '18 July 2021', '8:30 AM', '20 July 2021', 'Selangor');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tempahan`
+-- Indexes for table `kereta`
 --
-ALTER TABLE `tempahan`
-  ADD PRIMARY KEY (`idtempahan`);
+ALTER TABLE `kereta`
+  ADD PRIMARY KEY (`idkereta`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tempahan`
+-- AUTO_INCREMENT for table `kereta`
 --
-ALTER TABLE `tempahan`
-  MODIFY `idtempahan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `kereta`
+  MODIFY `idkereta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
