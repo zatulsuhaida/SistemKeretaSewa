ca0f0b6cdfe09559309c26614adfe15169a3525b
Bukankir18 <garenasyakir@gmail.com>
2021-09-18 19:54:22 +0800

    try edit index

--- database/sks.sql
+++ database/sks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2021 at 01:46 PM
+-- Generation Time: Sep 18, 2021 at 01:53 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
