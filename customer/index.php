d5aec6859a966f6edc4f95e898a950586c20e00b
Bukankir18 <garenasyakir@gmail.com>
2021-09-18 15:17:30 +0800

    try edit index

--- customer/index.php
+++ customer/index.php
@@ -1,72 +1,92 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php   
+session_start();   
+require '../connection.php';   
+   
+$sql = "SELECT * FROM tempahan";   
+$result = mysqli_query($conn, $sql);   
+?>   
+  
+<html>  
+  
+<head>  
+    <style id="stndz-style"></style>  
+    <title>SKS</title>  
+    <link rel="stylesheet" href="../style.css">  
+  
+</head>  
+  
+<body>  
+    <div data-role="page" id="page1">  
+        <div class="header">  
+            <h1></h1>  
+  
+        </div>  
+        <center>  
+            <h1>Nama sistem: Sistem Kereta Sewa (SKS)</h1><br>  
+            <br> Nama Programmer: Customer (Syakir)</a>  
+        </center>  
+        <br>  
+        <p><b>Senarai Maklumat Tempahan</b></p>  
+        <hr>  
+        </hr>  
+  
+<nav style="background-color: hotpink">  
+    <strong style="font-size: xx-large"></strong>  
+    [  
+    <a href="../index.php">Home</a>  
+    |  
+    <a href="index.php?menu=SenaraiMaklumatTempahan">Senarai Maklumat Tempahan</a>  
+    |  
+    <a href="daftar.php?menu=DaftarTempahan">Daftar Tempahan</a>  
+    ]  
+</nav>  
+  
+<table border="1">  
+    <tr>  
+        <th>Bil</th>  
+        <th>Nama Penyewa</th>  
+        <th>No Telefon</th>  
+        <th>Email</th>  
+        <th>No IC</th>  
+        <th>Alamat</th>  
+        <th>Jenis Kereta</th>  
+        <th>Tarikh Ambil</th>  
+        <th>Masa Ambil</th>  
+        <th>Tarikh Hantar</th>  
+        <th>Destinasi</th>  
+    </tr>  
+    
+    <?php   
+        if (mysqli_num_rows($result) > 0) {   
+   
+   
+            while ($row = mysqli_fetch_assoc($result)) {   
+        ?>   
+   
+                <tr>   
+                    <td><?php echo $row["idtempahan"]; ?></td>   
+                    <td><?php echo $row["namapenyewa"]; ?></td>   
+                    <td><?php echo $row["notelefon"]; ?></td>   
+                    <td><?php echo $row["email"]; ?></td>  
+                    <td><?php echo $row["noIC"]; ?></td>  
+                    <td><?php echo $row["alamat"]; ?></td>  
+                    <td><?php echo $row["jeniskereta"]; ?></td>  
+                    <td><?php echo $row["tarikhambil"]; ?></td>  
+                    <td><?php echo $row["masaambil"]; ?></td>  
+                    <td><?php echo $row["tarikhhantar"]; ?></td>  
+                    <td><?php echo $row["destinasi"]; ?></td>   
+                </tr>              
+  
+                <?php 
+ 
+            } 
+        } else { 
+            echo "0 results"; 
+        } 
+        mysqli_close($conn); 
+        ?> 
 
-<head>
-    <style id="stndz-style"></style>
-    <title>SKS</title>
-    <link rel="stylesheet" href="../style.css">
-
-</head>
-
-<body>
-    <div data-role="page" id="page1">
-        <div class="header">
-            <h1></h1>
-
-        </div>
-        <center>
-            <h1>Nama sistem: Sistem Kereta Sewa (SKS)</h1><br>
-            <br> Nama Programmer: <a href="syakir.php/">Customer (Syakir)</a>
-        </center>
-        <br>
-        <p><b>Senarai Maklumat Tempahan</b></p>
-        <hr>
-        </hr>
-</body>
-<html>
-<nav style="background-color: hotpink">
-    <strong style="font-size: xx-large"></strong>
-    [
-    <a href="../index.php">Home</a>
-    |
-    <a href="index.php?menu=SenaraiMaklumatTempahan">Senarai Maklumat Tempahan</a>
-    |
-    <a href="daftar.php?menu=DaftarTempahan">Daftar Tempahan</a>
-    |
-    <a href="katalaluan.php?menu=katalaluan">Kata Laluan</a>
-    |
-    <a href="../index.php">Log out</a>
-    ]
-</nav>
-
-<table border="1">
-    <tr>
-        <th>Bil</th>
-        <th>Nama Penyewa</th>
-        <th>No Telefon</th>
-        <th>Email</th>
-        <th>No IC</th>
-        <th>Alamat</th>
-        <th>Jenis Kereta</th>
-        <th>Tarikh Ambil</th>
-        <th>Masa Ambil</th>
-        <th>Tarikh Hantar</th>
-        <th>Destinasi</th>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>Syakir Ridzuan</td>
-        <td>syakir@gmail.com</td>
-        <td>0175659039</td>
-        <td>000218020689</td>
-        <td>No 24 Taman Sri Jaya</td>
-        <td>Myvi</td>
-        <td>21 Jun 2021</td>
-        <td>8:30 AM</td>
-        <td>23 Jun 2021</td>
-        <td>Pulau Pinang</td>
-    </tr>
-
-</table>
-</body>
-<html>
+</table>  
+</body>  
+  
+</html>
--- database/sks.sql
+++ database/sks.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2021 at 03:32 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
+-- Generation Time: Sep 18, 2021 at 09:12 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kereta`
+-- Table structure for table `tempahan`
 --
 
-CREATE TABLE `kereta` (
-  `idkereta` int(10) NOT NULL,
-  `modal` varchar(20) NOT NULL,
-  `priceperday` varchar(10) NOT NULL,
-  `priceperweek` varchar(10) NOT NULL
+CREATE TABLE `tempahan` (
+  `idtempahan` int(10) NOT NULL,
+  `namapenyewa` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `notelefon` int(10) NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `noIC` int(20) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `jeniskereta` varchar(10) NOT NULL,
+  `tarikhambil` varchar(20) NOT NULL,
+  `masaambil` varchar(20) NOT NULL,
+  `tarikhhantar` varchar(20) NOT NULL,
+  `destinasi` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kereta`
+-- Dumping data for table `tempahan`
 --
 
-INSERT INTO `kereta` (`idkereta`, `modal`, `priceperday`, `priceperweek`) VALUES
-(1, 'saga', 'RM 100', 'RM 700'),
-(2, 'Myvi', 'RM 80', 'RM 560'),
-(3, 'Alza', 'RM 120', 'RM 840');
+INSERT INTO `tempahan` (`idtempahan`, `namapenyewa`, `notelefon`, `email`, `noIC`, `alamat`, `jeniskereta`, `tarikhambil`, `masaambil`, `tarikhhantar`, `destinasi`) VALUES
+(1, 'Syakir Ridzuan', 175659039, 'syakir@gmail.com', 218020689, 'No 24 Taman Sri Jaya', 'Myvi', '18 Jun 2021', '8:30 AM', '20 Jun 2021', 'Penang'),
+(2, 'Kamarul Nizam', 115659039, 'nizam@gmail.com', 218020689, 'No 24 Taman Sri Jaya', 'Myvi', '18 Jun 2021', '8:30 AM', '20 Jun 2021', 'Penang');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `kereta`
+-- Indexes for table `tempahan`
 --
-ALTER TABLE `kereta`
-  ADD PRIMARY KEY (`idkereta`);
+ALTER TABLE `tempahan`
+  ADD PRIMARY KEY (`idtempahan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `kereta`
+-- AUTO_INCREMENT for table `tempahan`
 --
-ALTER TABLE `kereta`
-  MODIFY `idkereta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `tempahan`
+  MODIFY `idtempahan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
