e6bca4be2c36e9e298481e475e2ae8050591ce77
muhdradzi <aizawatakeru99@gmail.com>
2021-09-19 17:07:50 +0800

    try edit index

---
+++ manager/delete.php
@@ -0,0 +1,13 @@
+<?php 
+require '../connection.php'; 
+ 
+$idCustomer = $_GET['idCustomer']; 
+ 
+$deleteusr = "DELETE FROM customerlist WHERE idCustomer='$idCustomer'"; 
+$resultdelete -= $conn->query($deleteusr); 
+ 
+?> 
+ 
+<script> 
+    window.location = "index.php"; 
+</script>
